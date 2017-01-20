8f06b82793772900e347319aefb74beb5c2c9e2b
Scott Leland Crossen <scottcrossen42@gmail.com>
2017-01-19 19:51:44 -0700

    finished I think

--- effects.js
+++ effects.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
+
+    // The first few lines do the initialization whenever a fresh-load happens.
     console.log("JQuery Initialized");
-
-
-
+    // Randomize Board.
     var words = wordsJson.words;
     words.sort(function(a, b){return 0.5 - Math.random()});
-    
+    // Load Words into both possible places.
     console.log("Loading words:");
     for(var i = 0; i < words.length; i++) {
 	console.log("\t"+words[i]);
@@ -19,27 +19,31 @@ $(document).ready(function() {
 	$(button_element).appendTo("#word-button-"+String(Math.floor((i >= 13? i+1 : i)/5)));
     }
 
+    // The next few lines set listeners to various tasks.
+    // Set a window-resize listener.
     $(window).resize(function(){
-	//console.log("Resizing Screen");
+	// Set board length to board width.
 	$('.row .col-xs-2').each(function() {
-	    //console.log("Resizing Object with width",$(this).width());
             $(this).height($(this).width());
 	});
+	// If screen is small then display words below.
 	if($('.row .col-xs-2').width() <= 95){
 	    $('.row .col-xs-2 button p').hide();
 	    $('#word-list').show();
 	}
-	if($('.row .col-xs-2').width() > 95){
+	// If screen is big then display words on button.
+	else{
 	    $('.row .col-xs-2 button p').show();
 	    $('#word-list').hide();
 	}
-    }).resize();
+    }).resize(); // Perform resize.
+    // Add button-click listener.
     $("button").click(function(){
 	$(this).addClass("pushed");
 	console.log("Button pressed");
     });
+    // Remove bootstrap focus.
     $(".btn").mouseup(function(){
 	$(this).blur();
     })
-
 });
