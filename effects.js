22dfeecc695b6f57f09e4018cb5d3b2fcb1dbcec
Scott Leland Crossen <scottcrossen42@gmail.com>
2017-01-19 19:41:48 -0700

    fixed bug

--- effects.js
+++ effects.js
@@ -10,12 +10,12 @@ $(document).ready(function() {
     for(var i = 0; i < words.length; i++) {
 	console.log("\t"+words[i]);
 	var list_element="<li>"+words[i]+"</li>";
-	if(i==12) list_element="<li value=\"14\">"+words[i]+"</li>";
+	if(i==12) list_element="<li value=\"13\">"+words[i]+"</li>";
 	var button_element="<div class=\"col-xs-2 ";
 	if((i >= 13? i+1 : i)%5==0) button_element+="col-xs-offset-1";
 	button_element+="\"><button type=\"button\" class=\"btn btn-default\">"+String(i+1)+"<p>"+words[i]+"</p></button></div>";
-	$(list_element).appendTo("#word-list-"+String(Math.floor(i/8)));
 	if(i==12) button_element="<div class=\"col-xs-2\"><div style=\"position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%);\">Free!</div></div>\n"+button_element;
+	$(list_element).appendTo("#word-list-"+String(Math.floor(i/12)));
 	$(button_element).appendTo("#word-button-"+String(Math.floor((i >= 13? i+1 : i)/5)));
     }
 
