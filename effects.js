a4ff5e905479de7e9bcb1e264f5dab570c1227af
Scott Leland Crossen <scottcrossen42@gmail.com>
2017-01-19 18:59:28 -0700

    added dynamic screen resizing and text displaying.

--- effects.js
+++ effects.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
-    
     console.log("JQuery Initialized");
+
+
+
+    
+    console.log("Loading words:");
+    for(var i = 0; i < wordsJson.words.length; i++) {
+	console.log(wordsJson.words[i]);
+	var list_element="<li>"+wordsJson.words[i]+"</li>";
+	if(i==12) list_element="<li value=\"14\">"+wordsJson.words[i]+"</li>";
+	var button_element="<div class=\"col-xs-2 ";
+	if((i >= 13? i+1 : i)%5==0) button_element+="col-xs-offset-1";
+	button_element+="\"><button type=\"button\" class=\"btn btn-default\">"+String(i+1)+"<p>"+wordsJson.words[i]+"</p></button></div>";
+	$(list_element).appendTo("#word-list-"+String(Math.floor(i/8)));
+	if(i==12) button_element="<div class=\"col-xs-2\"><div style=\"position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%);\">Free!</div></div>\n"+button_element;
+	$(button_element).appendTo("#word-button-"+String(Math.floor((i >= 13? i+1 : i)/5)));
+    }
+
     $(window).resize(function(){
-	console.log("Resizing Screen");
+	//console.log("Resizing Screen");
 	$('.row .col-xs-2').each(function() {
-	    console.log("Resizing Object with width",$(this).width());
+	    //console.log("Resizing Object with width",$(this).width());
             $(this).height($(this).width());
 	});
+	if($('.row .col-xs-2').width() <= 95) $('.row .col-xs-2 button p').hide();
+	if($('.row .col-xs-2').width() > 95) $('.row .col-xs-2 button p').show();
     }).resize();
+    
 });
