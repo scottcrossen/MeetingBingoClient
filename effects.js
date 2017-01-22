22ec8b31f9d2be18f3b714d807e343ac450909b0
Scott Leland Crossen <scottcrossen42@gmail.com>
2017-01-21 17:37:11 -0700

    added win/lose conditions

--- effects.js
+++ effects.js
@@ -1,28 +1,79 @@
+// Initialize array to track which buttons are pressed.
 var pushed_array=Array(25).fill(false);
-pushed_array[13]=true;
+pushed_array[12]=true;
+
+// Randomize board.
+var words = wordsJson.words;
+words.sort(function(a, b){return 0.5 - Math.random()});
+
+// Define function to be called when an array button is pushed.
 function toggle_push(index){
     pushed_array[index]=!pushed_array[index];
 }
 
-$(document).ready(function() {
+// Define function to be called when bingo-button is pressed.
+function test_bingo(){
+    var found_bingo=false;
+    function win(){
+	console.log("\tBingo correct.");
+    }
+    function lose(){
+	console.log("\tBingo incorrect.");
+    }
+    // Test horizontal
+    for(var i=0; i<=4; i++){
+	for (var j=0; j<=4; j++){
+	    if (!pushed_array[j+i*5]) break;
+	    else if (j==4) found_bingo=true;
+	}
+    }
+    // Test vertical
+    for(var i=0; i<=4; i++){
+	for (var j=0; j<=4; j++){
+	    if (!pushed_array[j*5+i]) break;
+	    else if (j==4) found_bingo=true;
+	}
+    }
+    // Test diagonals
+    for(var i=0; i<=4; i++){
+	if(!pushed_array[i*6]) break;
+	else if (i==4) found_bingo=true;
+    }
+    for(var i=0; i<=4; i++){
+	if(!pushed_array[24-i*6]) break;
+	else if (i==4) found_bingo=true;
+    }
+    found_bingo? win() : lose();
+}
 
+// Define function to create a button and place it on the board.
+function create_button(i){
+    var button_element="<div class=\"col-xs-2 ";
+    if((i >= 13? i+1 : i)%5==0) button_element+="col-xs-offset-1";
+    button_element+="\"><button type=\"button\" class=\"btn btn-default\" data-id=\""+String(i >= 12? i+1 : i)+"\">"+String(i+1)+"<p>"+words[i]+"</p></button></div>";
+    if(i==12) button_element="<div class=\"col-xs-2\"><div style=\"position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%);\">Free!</div></div>\n"+button_element;
+    $(button_element).appendTo("#word-button-"+String(Math.floor((i >= 13? i+1 : i)/5)));
+}
+
+// Define function to create a list item for smaller screens.
+function create_list_item(i){
+    var list_element="<li>"+words[i]+"</li>";
+    if(i==12) list_element="<li value=\"13\">"+words[i]+"</li>";
+    $(list_element).appendTo("#word-list-"+String(Math.floor(i/12)));
+}
+
+// Define the "main" function.
+$(document).ready(function() {
+    
     // The first few lines do the initialization whenever a fresh-load happens.
     console.log("JQuery Initialized");
-    // Randomize Board.
-    var words = wordsJson.words;
-    words.sort(function(a, b){return 0.5 - Math.random()});
+    
     // Load Words into both possible places.
     console.log("Loading words:");
     for(var i = 0; i < words.length; i++) {
 	console.log("\t"+words[i]);
-	var list_element="<li>"+words[i]+"</li>";
-	if(i==12) list_element="<li value=\"13\">"+words[i]+"</li>";
-	var button_element="<div class=\"col-xs-2 ";
-	if((i >= 13? i+1 : i)%5==0) button_element+="col-xs-offset-1";
-	button_element+="\"><button type=\"button\" class=\"btn btn-default\" data-id=\""+String(i)+"\">"+String(i+1)+"<p>"+words[i]+"</p></button></div>";
-	if(i==12) button_element="<div class=\"col-xs-2\"><div style=\"position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%);\">Free!</div></div>\n"+button_element;
-	$(list_element).appendTo("#word-list-"+String(Math.floor(i/12)));
-	$(button_element).appendTo("#word-button-"+String(Math.floor((i >= 13? i+1 : i)/5)));
+	create_button(i);
+	create_list_item(i);
     }
 
     // The next few lines set listeners to various tasks.
@@ -43,19 +94,22 @@ $(document).ready(function() {
 	    $('#word-list').hide();
 	}
     }).resize(); // Perform resize.
+    
     // Add button-click listener for bingo board.
     $(".col-xs-2 button").click(function(){
 	$(this).toggleClass("pushed");
 	console.log("Button pressed with content: ");
 	console.log("\ttext: "+$(this).text());
-	console.log("\tdata: "+$(this).data("id"));
+	console.log("\tindex: "+$(this).data("id"));
 	toggle_push($(this).data("id"));
     });
+    
     // Add button-click listener for bingo board.
     $(".col-xs-10 button").click(function(){
-	$(this).toggleClass("pushed");
-	console.log("Button pressed");
+	console.log("Bingo button pressed");
+	test_bingo();
     });
+    
     // Remove bootstrap focus.
     $(".btn").mouseup(function(){
 	$(this).blur();
